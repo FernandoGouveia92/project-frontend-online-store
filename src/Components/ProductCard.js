20a6cfbd4a93718a72373fa9c9864a23c1567701
Henos <henosvinicius13@gmail.com>
2022-07-07 18:18:28 -0300

    Resolução do requisito 05

---
+++ src/Components/ProductCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends React.Component {
+  render() {
+    const {
+      image,
+      title,
+      price
+    } = this.props;
+
+    return (
+      <div data-testid="product">
+        <img src={ image } alt={ title } />
+        <p>{ title }</p>
+        <p>{ price }</p>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+export default ProductCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import ProductCard from '../Components/ProductCard';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productName: '',
+      products: [],
+      loading: false,
+      notFind: 'Digite algum termo de pesquisa ou escolha uma categoria.'
+    }
+  }
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name] : value,
+    });
+  }
+
+  handleClick = async () => {
+    const { productName } = this.state;
+    const getProducts = await getProductsFromCategoryAndQuery('', productName);
+    if(getProducts.results.length === 0) {
+      this.setState({
+        notFind: 'Nenhum produto foi encontrado',
+      });
+    } else {
+      this.setState({
+        products: getProducts.results,
+      });
+    }
+    console.log(getProducts.results);
+  }
+
+
   render() {
+    const {
+      productName,
+      notFind,
+      products,
+    } = this.state;
+
     return (
       <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
+        <input type="text" data-testid="query-input" name="productName" value={ productName } onChange={ this.handleChange } />
+        <button type="button" data-testid="query-button" onClick= { this.handleClick }>Pesquisar</button>
+        { notFind }
+        { products.map(({ price, title, thumbnail }, index) => <ProductCard image={ thumbnail } title={ title } price={ price } key={ index } />)}
       </div>
     );
   }
